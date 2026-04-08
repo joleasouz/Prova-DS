5b1d7abb358df0d6efe94a7c99fc4b6378667147
Julia Silva de Souza <juliasilvadesouza09@gmail.com>
2026-04-08 20:35:15 -0300

    add 2 exercicio completo

--- Ex2/src/Compra.java
+++ Ex2/src/Compra.java
@@ -1,6 +1,5 @@
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,5 +8,52 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Compra {
-    
+    JFrame janela;
+    JTextField valorC;
+    JTextField cupom;
+    JLabel textoValor;
+    JLabel textoCupom;
+    JPanel p;
+    JButton BtnEnviar;
+
+    public Compra(){
+        janela = new JFrame();
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLayout(new FlowLayout());
+        janela.setSize(400, 500);
+        janela.setLocationRelativeTo(null);
+        
+        textoValor = new JLabel("Valor da Compra: ");
+        valorC = new JTextField(25);
+
+        textoCupom = new JLabel("Cupom de Desconto: ");
+        cupom = new JTextField(25);
+
+        BtnEnviar = new JButton();
+        BtnEnviar.setText("Enviar");
+
+        BtnEnviar.addActionListener(e ->{
+            double v = Double.parseDouble(valorC.getText());
+            String c = cupom.getText();
+            if(v > 500){
+                v = v - (v*0.1);
+            }
+            if(c.trim().toUpperCase().equals("AMIGAO10")){
+                v = v - 10;
+            }
+            JOptionPane.showMessageDialog(null, "O valor da sua compra é: "+ v);
+        });
+        
+        p = new JPanel(new GridLayout(25,25));
+        p.add(textoValor);
+        p.add(valorC);
+        p.add(textoCupom);
+        p.add(cupom);
+        p.add(BtnEnviar);
+
+        janela.add(p);
+        janela.setVisible(true);
+    }
+
+
 }
